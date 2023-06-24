318efe4bf87f6b6e2c84a96f083863c92e5e5fa2
sako <123950093+ayasako@users.noreply.github.com>
2023-06-24 21:12:43 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+console.log("calc");
+
+let num=90;
+
+if( ( num % 15 ) === 0 ) {
+    console.log('3と5の倍数です');
+}
+
+//3の倍数の場合
+else if( ( num % 3 ) === 0 ) {
+    console.log('3の倍数です');
+}
+
+//5の倍数の倍数
+else if( ( num % 5 ) === 0 ) {
+    console.log( '5の倍数です');
+}
