18786ce077cf956557bb6df07aa368920e11c1fc
sako <123950093+ayasako@users.noreply.github.com>
2023-06-28 16:51:59 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,8 +0,0 @@
-// textというidを持つHTML要素を取得し、定数に代入
-btn.addEventListener('click', () => {
-    setTimeout(() => {
-    const text = document.getElementById('text').textContent = 'ボタンをクリックしました';
-    console.log('ボタンをクリックしました');
-    }, 2000);
-});
-
